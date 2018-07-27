13ef04a15aaf5ae9803751fef364ba93c076fb0b
adamclmns <adam@adamclmns.com>
2018-07-27 17:24:06 -0500

    Minor improvements to RPS

--- RPS/src/main/java/edu/asu/rps/Main.java
+++ RPS/src/main/java/edu/asu/rps/Main.java
@@ -5,6 +5,7 @@
  */
 package edu.asu.rps;
 
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,68 +15,88 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("--- Rock, Paper, Scissors ---");
+    static Scanner scanner = new Scanner(System.in);
+    static HashMap<Integer, String> options = new HashMap<>();
 
+    public static void main(String[] args) {
+        boolean userContinues = true;
+        String playerName;
+        // Populate the options Map
+        options.put(1, "ROCK");
+        options.put(2, "PAPER");
+        options.put(3, "SCISSORS");
+
+        System.out.println("--- Rock, Paper, Scissors ---");
 //        Prompt for the player’s name
         System.out.println("What's your name");
-        String playerName = scanner.next();
+        playerName = scanner.next();
 
-        boolean userContinues = true;
 //       Enter game loop (do{game} while ( userContinues == true )
         do {
-            //            Prompt user for choice (rock, paper, scissors)
-            System.out.println("Rock, Paper, or Scissors?");
-            String userChoice = scanner.next();
-            //            Program randomly chooses (rock, paper, scissors)
-            // https://stackoverflow.com/a/363692 
-            Random rand = new Random();
-            int compChoice = rand.nextInt(3)+1;
-            System.out.println("Random Number: " + compChoice);
-            // 1 == Rock
-            // 2 == Paper
-            // 2 == Scissors
-            //            Evaluate outcome. 
-            if (userChoice.equalsIgnoreCase("ROCK")) {
-                if (compChoice == 1) {
-                    System.out.println("TIE");
-                } else if (compChoice == 2) {
+            // Get User Choice
+            String userChoice = getUserChoice();
+            // Get Computer Choice
+            String compChoice = getComputerChoice();
+            //            Evaluate outcome.
+
+            // If Tie, let's figure it out now.
+            if (userChoice.equalsIgnoreCase(compChoice)) {
+                System.out.println("It's a Tie!");
+            } else if (userChoice.equalsIgnoreCase("ROCK")) {
+                if (compChoice.equalsIgnoreCase("PAPER")) {
                     System.out.println("Computer Wins!");
-                } else if (compChoice == 3) {
-                    System.out.println("User Wins!");
+                } else if (compChoice.equalsIgnoreCase("SCISSORS")) {
+                    System.out.println(playerName + " Wins!");
                 }
             } else if (userChoice.equalsIgnoreCase("PAPER")) {
-                if (compChoice == 1) {
-                    System.out.println("User Wins!");
-                } else if (compChoice == 2) {
-                    System.out.println("TIE");
-                } else if (compChoice == 3) {
+                if (compChoice.equalsIgnoreCase("ROCK")) {
+                    System.out.println(playerName + " Wins!");
+                }
+                if (compChoice.equalsIgnoreCase("SCISSORS")) {
                     System.out.println("Computer Wins!");
                 }
             } else if (userChoice.equalsIgnoreCase("SCISSORS")) {
-                if (compChoice == 1) {
-                       System.out.println("Computer Wins");
-                } else if (compChoice == 2) {
-                        System.out.println("User wins");
-                } else if (compChoice == 3) {
-                    System.out.println("TIE");
+                if (compChoice.equalsIgnoreCase("ROCK")) {
+                    System.out.println("Computer Wins");
+                } else if (compChoice.equalsIgnoreCase("PAPER")) {
+                    System.out.println(playerName + " wins");
                 }
             }
-                
-            //            Output the outcome.
-            
-            //            Ask user if they want to continue (userContinues)
-            System.out.println("Do you want to continue playing?");
-            System.out.println("Y or N");
-            String keepPlaying = scanner.next();
-            if(keepPlaying.equalsIgnoreCase("Y")){
-                userContinues = true;
-            }else {
-                userContinues = false;
-            }
+
+            //            Output the outcome. 
+            userContinues = getUserContinues();
         } while (userContinues);
 
 //        Exit.
     }
+
+    public static boolean getUserContinues() {
+        //            Ask user if they want to continue (userContinues)
+        System.out.println("Do you want to continue playing?");
+        System.out.println("Y or N");
+        String keepPlaying = scanner.next();
+
+        if (keepPlaying.equalsIgnoreCase("Y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String getUserChoice() {
+        //            Prompt user for choice (rock, paper, scissors)
+        System.out.println("Rock, Paper, or Scissors?");
+        String userChoice = scanner.next();
+        return userChoice;
+    }
+
+    public static String getComputerChoice() {
+        //            Program randomly chooses (rock, paper, scissors)
+        // https://stackoverflow.com/a/363692 
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(3) + 1;
+        String compChoice = options.get(randomNumber);
+        return compChoice;
+    }
+
 }
